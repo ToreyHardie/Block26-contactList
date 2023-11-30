2a1fc9a98c00aed34b893c045501d699729c70a5
Torey Hardie <Torey.walker@gmail.com>
2023-11-30 12:04:03 -0600

    Initial commit

---
+++ contactList/src/components/ContactRow.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ContactList from './ContactList';
+
+function ContactRow({ setSelectedContactId, contact}) {
+  return (
+    <tr
+      onClick={() => {
+        setSelectedContactId(contact.id);
+      }}
+    >
+      <td>{contact.name}</td>
+      <td>{contact.email}</td>
+      <td>{contact.phone}</td>
+    </tr>
+  );
+}
+
+export default ContactRow
---
+++ contactList/src/components/SelectedContact.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SelectedContact() {
+  return (
+    <div>SelectedContact</div>
+  )
+}
+
+export default SelectedContact
